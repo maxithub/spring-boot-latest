d35008f5af7f714c6377c37cb2021b5f064c94a4
Max Yuan <max.m.yuan@gmail.com>
2019-05-05 10:11:23 +0800

    Add the simplest oauth2 server with just one client

---
+++ oauth2-server-web/src/test/java/max/lab/springboot/latest/oauth2serverweb/Oauth2ServerWebApplicationTests.java
@@ -0,0 +1,16 @@
+package max.lab.springboot.latest.oauth2serverweb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Oauth2ServerWebApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
